760522d4bb977ca36be186ff4442cea989d822cc
Daiju Nakayama <daiju.nakayama@pixiedusttech.com>
2019-05-29 20:36:24 +0900

    Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use wasm_bindgen::prelude::wasm_bindgen;
+
+#[wasm_bindgen]
+pub fn add(a: f64, b: f64) -> f64 {
+    a + b
 }
