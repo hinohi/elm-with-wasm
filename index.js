817f5a2834ccdb26f71d31cf14f64eef4fda6670
Daiju Nakayama <daiju.nakayama@pixiedusttech.com>
2019-05-31 13:59:54 +0900

    index.jsでの初期化を安全に

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const rust = import('./pkg/elm_wasm_test');
 
-let wasm = {};
-
 const app = Elm.Main.init({
     node: document.getElementById('elm'),
 });
 
-app.ports.send.subscribe(args => {
-        let [a, b] = args;
-        let ans = wasm.add(a, b);
-        app.ports.rev.send(ans);
-    }
-);
-
 async function run() {
-    wasm = await rust;
+    let wasm = await rust;
+    app.ports.send.subscribe(args => {
+            let [a, b] = args;
+            let ans = wasm.add(a, b);
+            app.ports.rev.send(ans);
+        }
+    );
 }
 
 run().catch(console.error);
